4f37a43d5ff6d4daea41b715c3c0906f039787b3
Ulada Haranina <ulada.haranina@revature.com>
2024-06-05 14:44:28 -0400

    add random bg selection

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import {NgFor, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-record',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NgFor, NgStyle],
   templateUrl: './record.component.html',
   styleUrl: './record.component.css'
 })
 export class RecordComponent {
-@Input() patients: any
+@Input() patients: any[] = [];
+
+ blueShades: string[] = [
+  'rgba(255,39,169, 0.1) ',  // Base blue color
+  'rgba(95,39,255, 0.1) ',  // Darker shade of blue
+  'rgba(255,39,39, 0.1)',  // Even darker shade of blue
+  'rgba(44,39,255, 0.1)',  // Darker shade of blue
+  'rgba(57,255,39, 0.1)'   // Darkest shade of blue
+];
+
+//Get random color
+applyColor = () => {
+  const index: number = Math.floor(Math.random() * this.blueShades.length);
+  return this.blueShades[index];
+}
 }
